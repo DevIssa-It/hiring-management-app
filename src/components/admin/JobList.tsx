b8266549a212df9f93a9bc4f08e69576ddf47730
Qiforiss67 <issaitama07@gmail.com>
2025-11-16 23:58:57 +0700

    refactor: Change JobList layout from grid to flex for better responsiveness

--- src/components/admin/JobList.tsx
+++ src/components/admin/JobList.tsx
@@ -9,7 +9,7 @@ export interface JobListProps {
 
 export const JobList: React.FC<JobListProps> = ({ jobs, onJobClick, onStatusChange }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="flex flex-col gap-4">
       {jobs.map((job) => (
         <JobCard 
           key={job.id} 
